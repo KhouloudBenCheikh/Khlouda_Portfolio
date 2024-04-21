edceaf3f7b48f82f6ae54aa65623861ddd018eb3
KhouloudBenCheikh <khouloudbencheikh011@gmail.com>
2024-04-21 16:10:02 +0200

    fixed an issue

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Socials from "../components/socials";
+import Socials from "../components/Socials";
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
